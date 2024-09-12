208527a76ba36fbd675caa7e6266413c751cc316
Joaquin Bianciotto <joaquinbianciotto123@hotmail.com>
2024-09-12 15:49:48 -0300

    logica basica del cpu

--- src/cpu.py
+++ src/cpu.py
@@ -1,1 +1,27 @@
 #clase cpu
+class cpu:
+    #constructor
+    def __init__(self):
+        self.proceso = None
+        self.tiempoRestante = 0
+    #metodo que asigna un proceso al cpu
+    def asignarProceso(self, proceso):
+        self.procesoActual = proceso
+        self.tiempoRestante = proceso.tiempo
+    #metodo que ejecuta un ciclo de reloj
+    def ejecutar(self):
+        if self.procesoActual != None:
+            self.tiempoRestante -= 1
+            if self.tiempoRestante == 0:
+                self.procesoActual = None
+        else:
+            print("CPU: No hay proceso en ejecucion")
+    #metodo que retorna el proceso actual
+    def getProcesoActual(self):
+        return self.procesoActual
+    #metodo que retorna el tiempo restante
+    def getTiempoRestante(self):
+        return self.tiempoRestante
+    #metodo que retorna si el cpu esta ocupado
+    def estaOcupado(self):
+        return self.procesoActual != None
