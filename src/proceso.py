939095792c213754c527056e41971115f8b3a2dd
Joaquin Bianciotto <joaquinbianciotto123@hotmail.com>
2024-09-12 15:56:07 -0300

    clase proceso

--- src/proceso.py
+++ src/proceso.py
@@ -1,1 +1,17 @@
 #clase proceso
+class proceso:
+    #constructor
+    restante = 0
+    def __init__(self, PID, TI, tamaño):
+        self.PID = PID
+        self.TI = TI
+        self.tamaño = tamaño
+        self.restante = TI
+
+    # getter for tamaño
+    def get_tamaño(self):
+        return self.tamaño
+
+    # setter for tamaño
+    def set_tamaño(self, nuevo_tamaño):
+        self.tamaño = nuevo_tamaño
