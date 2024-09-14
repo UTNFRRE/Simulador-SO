3586ec33c86713c1d58866b1f52849d7b2a6a261
yoelmarain <jmarain62@gmail.com>
2024-09-14 00:10:02 -0300

    Refactor PlanificadorLargo class to implement Worst Fit algorithm for partition allocation

--- src/planLargo.py
+++ src/planLargo.py
@@ -1,1 +1,31 @@
 #clase planLargo (planificador a latgo plazo)
+
+
+# 1- Worst fit 
+# asignar la particion 
+# manejar la cola de procesos nuevos
+
+class PlanificadorLargo:
+
+
+    def __init__(self, memoria, procesos):
+        self.memoria = memoria
+        self.procesos = procesos
+        self.cola_nuevos = []
+        self.cola_listos = []
+
+    def WorstFit(self, proceso):
+        particion_elegida = None
+        particiones = self.memoria.getParticiones()
+        max_fragmentacion = -1
+        for particion in particiones:
+            if not particion.ocupado and particion.tamaño >= proceso.tamaño:
+                fragmentacion = particion.tamaño - proceso.tamaño
+                if fragmentacion > max_fragmentacion:
+                    max_fragmentacion = fragmentacion
+                    particion_elegida = particiones.index(particion)
+        if particion_elegida:
+            self.memoria.añadirProceso(proceso, particion_elegida)    # pedir a memoria q añada, y memoria pide a particion
+            proceso.set_particion(particion_elegida)
+            return True
+        return False
