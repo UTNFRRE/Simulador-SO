411e6271367d70328fd72366eb7e450e0150dd8e
yoelmarain <134217892+yoelmarain@users.noreply.github.com>
2024-11-10 12:38:37 -0300

    fix: agregar unidades a los tamaños en la tabla de particiones y cola de procesos

--- src/memoria.py
+++ src/memoria.py
@@ -100,7 +100,7 @@ class memoria:
         print("Tabla de particiones:")
         data=[]	
         for particion in self.getParticiones():
-            particion_info = [f"{particion.tamaño}K",f"{particion.dirInicio}K", Fore.RED + 'Ocupado' + Fore.RESET if particion.proceso else Fore.GREEN + 'Libre' + Fore.RESET, particion.proceso.PID if particion.proceso else '-', particion.fragmentacionInterna]
+            particion_info = [f"{particion.tamaño}K",f"{particion.dirInicio}K", Fore.RED + 'Ocupado' + Fore.RESET if particion.proceso else Fore.GREEN + 'Libre' + Fore.RESET, particion.proceso.PID if particion.proceso else '-', f"{particion.fragmentacionInterna}K"]
             data.append(particion_info)
            # print(f"Partición {particion.tamaño}K en {particion.dirInicio}K: Proceso {particion.proceso.PID if particion.proceso else 'Ninguno'}, Fragmentación {particion.fragmentacionInterna}K")
         print(tabulate(data, headers=headers, tablefmt='grid'))
@@ -109,11 +109,11 @@ class memoria:
         data = []
         if self.cola_listos[0]:
             for proceso in self.cola_listos[0]:
-                data.append([proceso.PID, proceso.tiempoArribo, proceso.tamaño, proceso.tiempoRestante,Fore.MAGENTA +  "Memoria" + Fore.RESET])
+                data.append([proceso.PID, proceso.tiempoArribo, f"{proceso.tamaño}K", proceso.tiempoRestante,Fore.MAGENTA +  "Memoria" + Fore.RESET])
             
         if self.cola_listos[1]:
             for proceso in self.cola_listos[1]:
-                data.append([proceso.PID, proceso.tiempoArribo, proceso.tamaño, proceso.tiempoRestante,Fore.BLUE +  "Disco" + Fore.RESET])  
+                data.append([proceso.PID, proceso.tiempoArribo, f"{proceso.tamaño}K", proceso.tiempoRestante,Fore.BLUE +  "Disco" + Fore.RESET])  
         
         if data:
             print("Cola de procesos listos:")
